023ec429f25474347da4bf1d6a6743f7a0e10304
Mahdisamiee <34692316+Mahdisamiee@users.noreply.github.com>
2020-01-20 19:34:04 +0330

    updtede 30/10-2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,29 @@
 
+import lulu2_package.*;
 
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenFactory;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.UnbufferedTokenStream;
+import org.antlr.v4.runtime.CommonToken;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
-public class Main {
+public class Main extends lulu2_grammerBaseVisitor{
     public static void main(String[] args) throws Exception {
+        String st = "whu";
 
+        CharStream input = CharStreams.fromString(st);
+        lulu2_grammerLexer lexer = new lulu2_grammerLexer(input);
+        lexer.setTokenFactory(new CommonTokenFactory(true));
+        TokenStream tokens = new UnbufferedTokenStream<CommonToken>(lexer);
+        lulu2_grammerParser parser = new lulu2_grammerParser(tokens);
+        parser.setBuildParseTree(true);
+        ParseTree tree = parser.program();
+
+        lulu2_grammerListener listener = new lulu2_grammerBaseListener();
+        ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk(listener, tree);
     }
 }
